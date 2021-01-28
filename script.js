7b6765e0c31ad9d6ab1729b234705e7c938c49f2
tod619 <trevorodwyer619@hotmail.com>
2021-01-28 13:44:21 +0000

    add EL to section elements to close effect on click in script

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ document.querySelector('.menu').addEventListener('click', () => {
         item.classList.toggle('change')
     })
 });
+
+// remove chnge from all when user clicks on the section wrappers
+document.querySelectorAll('.wrapper').forEach((item) => {
+    item.addEventListener('click', () => {
+        document.querySelectorAll('.target').forEach((item) => {
+            item.classList.remove('change')
+        })
+    });
+});
